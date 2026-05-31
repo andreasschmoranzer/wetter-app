15616ced3dae1e97393b62fb797d815b01666d5c
andreas schmoranzer <andreas.schmoranzer@web.de>
2026-05-31 21:36:58 +0200

    Test2

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://api.weatherapi.com/v1";
+const API_BASE_URL = "https://api.weatherapi.com/v1";
 const API_KEY = "a52a3c13c4f94a06810125930262404";
 
 export async function getCurrentWeatherAndForecast(location, days = 3) {
