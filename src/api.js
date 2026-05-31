bdcbabcdb7e565afc508483b6350569f508f7400
andreas schmoranzer <andreas.schmoranzer@web.de>
2026-05-31 21:33:49 +0200

    test

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-import { API_BASE_URL, API_KEY } from "./main";
+const API_BASE_URL = "http://api.weatherapi.com/v1";
+const API_KEY = "a52a3c13c4f94a06810125930262404";
 
 export async function getCurrentWeatherAndForecast(location, days = 3) {
   const response = await fetch(
--- src/main.js
+++ src/main.js
@@ -6,7 +6,4 @@ export const rootEl = document.getElementById("app");
 export const LOCALSTORAGE_KEY = "favorites";
 export const SHOW_SEARCHRESULTS = "show-search-data";
 
-export const API_BASE_URL = "http://api.weatherapi.com/v1";
-export const API_KEY = "a52a3c13c4f94a06810125930262404";
-
 loadMainMenu();
